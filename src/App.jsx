f1491b8e8cdbf9d3850189eadabf2aad434ec6a9
luis <luis.barrientos.faj@gmail.com>
2025-05-28 15:51:15 -0400

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './pages/LoginPage';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <LoginPage />
+    <>
+      <LoginPage />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; 2025 Luis Barrientos. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
